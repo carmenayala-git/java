d35d47c4b4842c22fab95439207f94eb09d956ec
carmen <carmenayalao1231@gmail.com>
2020-05-06 00:35:51 -0400

    commit inicial_javaciclos

---
+++ Ciclos.java
@@ -0,0 +1,45 @@
+public class  Ciclos{
+    public static void main(String[] args){
+        int i =0;
+        System.out.println("Serie con for: ");
+        for(i = 1;i<=10; i++){
+            if(i<10){
+                System.out.print(i + ",");
+
+            } else {
+               System.out.print(i); 
+            }
+
+        }
+        i=1;
+        System.out.println("");
+
+        System.out.println("Serie con while: ");
+        while(i<=10){
+            if(i<10){
+                System.out.print(i + ",");
+
+            } else {
+               System.out.print(i); 
+            }
+i++;
+        }
+        i++;
+        System.out.println("");
+
+        System.out.println("Serie con do-while: ");
+        do{
+            if(i<10){
+                System.out.print(i + ",");
+
+            } else {
+               System.out.print(i); 
+            }
+i++;
+
+        }while (i<=10);
+
+
+
+    }
+}
